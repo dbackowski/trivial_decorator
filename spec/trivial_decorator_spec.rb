a1b9748a960fe647c35a4b799a0e0881ddb37288
Damian Baćkowski <damianbackowski@gmail.com>
2017-12-04 17:32:27 +0100

    add spec dummy app

--- spec/trivial_decorator_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe TrivialDecorator do
-  it "has a version number" do
-    expect(TrivialDecorator::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
